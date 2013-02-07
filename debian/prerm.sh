2f06edc275c0077860e0be83c30d8a77fb3001b5
Ozan Turksever <ozan.turksever@innotim.com>
2013-02-07 16:02:27 +0200

    remove pyc before remove

--- debian/prerm.sh
+++ debian/prerm.sh
@@ -2,7 +2,6 @@
 
 case "$1" in
     remove)
-        find /usr/local/lib/python2.7/dist-packages/pysamba -name *.pyc -exec rm {} \;
-        find /usr/local/lib/python2.7/dist-packages/zenoss_utils -name *.pyc -exec rm {} \;
+        find /usr/local/lib/python2.7/dist-packages/logbook -name *.pyc -exec rm {} \;
     ;;
 esac
